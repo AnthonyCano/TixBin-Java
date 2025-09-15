05eef3107c7de25b30373ec9373a58223cff2c5e
AnthonyCano <132002947+AnthonyCano@users.noreply.github.com>
2025-09-15 16:20:14 -0700

    starting fresh again with gradle and new packages

---
+++ TixBin/src/main/java/com/tixbin/tixbinspring/TixBinApplication.java
@@ -0,0 +1,13 @@
+package com.tixbin.tixbinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TixBinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TixBinApplication.class, args);
+	}
+
+}
---
+++ TixBin/src/test/java/com/tixbin/tixbinspring/TixBinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tixbin.tixbinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TixBinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
